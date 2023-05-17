b6d610ecd5394408040c3d1f332472b392f9410b
Vladimir RM <shvdrummer@gmail.com>
2023-05-17 13:35:21 +0300

    Adding  main redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './App';
+import { rootReducer } from './redux/reducers/rootReducer';
 
 const store = createStore(rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
 const app = (
   <Provider store={store}>
   <App />
-  </Provider>
-)
+</Provider>
+);
 
-ReactDOM.render(app,document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
