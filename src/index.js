b73cce013519e583d38c237efdc81c2c40f75e89
Vladimir RM <shvdrummer@gmail.com>
2023-05-17 13:27:45 +0300

    Change App

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const store = createStore(rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+const app = (
+  <Provider store={store}>
+  <App />
+  </Provider>
+)
+
+ReactDOM.render(app,document.getElementById('root'));
